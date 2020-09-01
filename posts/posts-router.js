56120ff8d12bf3dc9b82af6fd5954feb2944f66a
DeeDowns <dee_downs@outlook.com>
2020-09-01 17:01:42 -0400

    GET comment by post id complete

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -28,8 +28,22 @@ router.get('/:id', (req, res) => {
     .catch(error => {
         res.status(500).json({ error: 'The post information could not be retrieved.'})
     })
-
  
+})
+
+//GET post comments
+router.get('/:id/comments', (req, res) => {
+    Posts.findPostComments(req.params.id)
+    .then(comment => {
+        if(comment) {
+            res.status(200).json(comment)
+        } else {
+            res.status(404).json({ message: 'The post with the specified ID does not exist.'})
+        }
+    })
+    .catch(error => {
+        res.status(500).json({ error: 'The comments information could not be retrieved.'})
+    })
 })
 
 module.exports = router
