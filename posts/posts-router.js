2a4ccedb4206d98d74160839ebdbd71f9ccc6a51
DeeDowns <dee_downs@outlook.com>
2020-09-01 16:56:18 -0400

    GET post by id complete

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 const Posts = require('../data/db.js')
 
-
+//GET all posts 
 router.get('/', (req, res) => {
     Posts.find()
     .then(posts => {
@@ -13,6 +13,23 @@ router.get('/', (req, res) => {
     .catch(error => {
         res.status(500).json({ error: 'The posts information could not be retrieved.'})
     })
+})
+
+//GET all posts by id
+router.get('/:id', (req, res) => {
+    Posts.findById(req.params.id)
+    .then(post => {
+        if(post) {
+            res.status(200).json(post)
+        } else {
+            res.status(404).json({ message: 'The post with the specified ID does not exist.'})
+        }
+    })
+    .catch(error => {
+        res.status(500).json({ error: 'The post information could not be retrieved.'})
+    })
+
+ 
 })
 
 module.exports = router
