42d55ad05ca69be3596e4c68028d8d78c8093d36
DeeDowns <dee_downs@outlook.com>
2020-09-01 17:59:33 -0400

    DELETE post complete

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -60,4 +60,32 @@ router.post('/', (req, res) => {
 
 })
 
+//POST new comment post by id
+router.post('/:id/comments', (req, res) => {
+    Posts.insertComment(req.body)
+    .then(comment => {
+        console.log(req.body)
+        req.body.text ? res.status(201).json(comment) : res.status(400).json({ message: 'Please provide text for the comment.'})
+        // req.body. ? res.status(201).json(comment) : res.status(400).json({ message: 'Please provide text for the comment.'})
+    })
+    .catch(error => {
+        res.status(500).json({ error: "There was an error while saving the comment to the database"})
+    })
+})
+
+//DELETE post
+router.delete('/:id', (req, res) => {
+    Posts.remove(req.params.id)
+    .then(post => {
+        if(post) {
+            res.status(200).json({ message: 'the post has been removed'})
+        } else {
+            res.status(404).json({ message: 'The post with the specified ID does not exist.'})
+        }
+    })
+    .catch(error => {
+        res.status(500).json({ error: 'The post could not be removed'})
+    })
+})
+
 module.exports = router
