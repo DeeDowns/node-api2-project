f16811e6bff10b742d35e8b4192aad2d64f6f5ca
DeeDowns <dee_downs@outlook.com>
2020-09-01 16:49:02 -0400

    GET posts request complete

---
+++ posts/posts-router.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const router = express.Router()
+
+const Posts = require('../data/db.js')
+
+
+router.get('/', (req, res) => {
+    Posts.find()
+    .then(posts => {
+        res.status(200).json(posts)
+    })
+    .catch(error => {
+        res.status(500).json({ error: 'The posts information could not be retrieved.'})
+    })
+})
+
+module.exports = router
